5d370fe4770f870fbae5b1558638724a3b44c25b
Jacson R. Sasse <jacson.sasse@hotmail.com>
2025-11-02 00:17:02 -0300

    feat: created section title component and used those components in index file

--- web/src/routes/index.tsx
+++ web/src/routes/index.tsx
@@ -1,12 +1,33 @@
 import { createFileRoute } from "@tanstack/react-router";
 import { Panel, PanelGroup, PanelResizeHandle } from "react-resizable-panels";
 import { Sidebar } from "../components/sidebar";
+import { WebhookDetailHeader } from "../components/webhook-detail-header";
+import { SectionTitle } from "../components/section-title";
 
 export const Route = createFileRoute("/")({
   component: Index,
 });
 
 function Index() {
+  const overviewData = [
+    {
+      key: "Method",
+      value: "POST",
+    },
+    {
+      key: "Status Code",
+      value: "200",
+    },
+    {
+      key: "Content-type",
+      value: "application/json",
+    },
+    {
+      key: "Content-length",
+      value: "28794 bytes",
+    },
+  ];
+
   return (
     <div className="h-screen bg-zinc-900">
       <PanelGroup direction="horizontal">
@@ -16,7 +37,21 @@ function Index() {
 
         <PanelResizeHandle className="w-px bg-zinc-700 hover:bg-zinc-600 transition-colors duration-150" />
 
-        <Panel defaultSize={80} minSize={60}></Panel>
+        <Panel defaultSize={80} minSize={60}>
+          <div className="flex h-full flex-col">
+            <WebhookDetailHeader />
+
+            <div className="flex-1 overflow-y-auto">
+              <div className="space-y-6 p-6">
+                <div className="space-y-4">
+                  <SectionTitle>Request Overview</SectionTitle>
+                </div>
+
+                <div></div>
+              </div>
+            </div>
+          </div>
+        </Panel>
       </PanelGroup>
     </div>
   );
