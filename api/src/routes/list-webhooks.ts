f59c76ae28637a495fc2193e2902f6871bcd7b02
Jacson R. Sasse <jacson.sasse@hotmail.com>
2025-11-02 01:22:32 -0300

    refactor: list webhooks

--- api/src/routes/list-webhooks.ts
+++ api/src/routes/list-webhooks.ts
@@ -1,5 +1,9 @@
+import { createSelectSchema } from "drizzle-zod";
 import { FastifyPluginAsyncZod } from "fastify-type-provider-zod";
 import { z } from "zod";
+import { webhooks } from "../db/schema";
+import { db } from "../db";
+import { desc, lt } from "drizzle-orm";
 
 export const listWebhooks: FastifyPluginAsyncZod = async (app) => {
   app.get(
@@ -10,24 +14,46 @@ export const listWebhooks: FastifyPluginAsyncZod = async (app) => {
         tags: ["webhook"],
         querystring: z.object({
           limit: z.coerce.number().min(1).max(100).default(20),
+          cursor: z.string().optional(),
         }),
         response: {
-          200: z.array(
-            z.object({
-              id: z.string(),
-              method: z.string(),
-            })
-          ),
+          200: z.object({
+            webhooks: z.array(
+              createSelectSchema(webhooks).pick({
+                id: true,
+                method: true,
+                pathname: true,
+                createdAt: true,
+              })
+            ),
+            nextCursor: z.string().nullable(),
+          }),
         },
       },
     },
     async (request, reply) => {
-      return [
-        {
-          id: "1",
-          method: "POST",
-        },
-      ];
+      const { limit, cursor } = request.query;
+
+      const result = await db
+        .select({
+          id: webhooks.id,
+          method: webhooks.method,
+          pathname: webhooks.pathname,
+          createdAt: webhooks.createdAt,
+        })
+        .from(webhooks)
+        .where(cursor ? lt(webhooks.id, cursor) : undefined)
+        .orderBy(desc(webhooks.id))
+        .limit(limit + 1);
+
+      const hasNext = result.length > limit;
+      const items = hasNext ? result.slice(0, limit) : result;
+      const nextCursor = hasNext ? items[items.length - 1].id : null;
+
+      reply.send({
+        webhooks: items,
+        nextCursor,
+      });
     }
   );
 };
